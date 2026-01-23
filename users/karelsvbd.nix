64d4b58eec7ba50bc2684ce7edb532f6120436b9
Relka-Dev <relka.dev@gmail.com>
2026-01-23 18:53:10 +0100

    Update to Fossil watch inspired colors

--- users/karelsvbd.nix
+++ users/karelsvbd.nix
@@ -11,55 +11,55 @@
   home-manager.users.karelsvbd = { pkgs, ... }: {
     imports = [ ./common.nix ];
 
-    # Override: thème Leather Gold (noir/brun cuir/doré)
+    # Override: thème inspiré montre Fossil (brun chocolat/cuir + or champagne)
     programs.plasma = {
       workspace.colorScheme = "BreezeDark";
       
       configFile = {
-        # Couleurs principales - tons bruns/cuir
-        "kdeglobals"."Colors:Window"."BackgroundNormal" = "25,22,20";
-        "kdeglobals"."Colors:Window"."BackgroundAlternate" = "30,27,24";
-        "kdeglobals"."Colors:Window"."ForegroundNormal" = "230,220,205";
-        "kdeglobals"."Colors:Window"."ForegroundInactive" = "120,105,95";
+        # Couleurs principales - tons chocolat/brun profond
+        "kdeglobals"."Colors:Window"."BackgroundNormal" = "28,22,16";
+        "kdeglobals"."Colors:Window"."BackgroundAlternate" = "35,28,20";
+        "kdeglobals"."Colors:Window"."ForegroundNormal" = "212,197,176";
+        "kdeglobals"."Colors:Window"."ForegroundInactive" = "120,100,80";
         
-        "kdeglobals"."Colors:View"."BackgroundNormal" = "18,16,14";
-        "kdeglobals"."Colors:View"."BackgroundAlternate" = "28,25,23";
-        "kdeglobals"."Colors:View"."ForegroundNormal" = "230,220,205";
-        "kdeglobals"."Colors:View"."ForegroundInactive" = "120,105,95";
+        "kdeglobals"."Colors:View"."BackgroundNormal" = "20,16,11";
+        "kdeglobals"."Colors:View"."BackgroundAlternate" = "30,24,18";
+        "kdeglobals"."Colors:View"."ForegroundNormal" = "212,197,176";
+        "kdeglobals"."Colors:View"."ForegroundInactive" = "120,100,80";
         
-        "kdeglobals"."Colors:Button"."BackgroundNormal" = "35,30,27";
-        "kdeglobals"."Colors:Button"."BackgroundAlternate" = "28,25,23";
-        "kdeglobals"."Colors:Button"."ForegroundNormal" = "230,220,205";
-        "kdeglobals"."Colors:Button"."ForegroundInactive" = "120,105,95";
+        "kdeglobals"."Colors:Button"."BackgroundNormal" = "40,32,24";
+        "kdeglobals"."Colors:Button"."BackgroundAlternate" = "32,26,19";
+        "kdeglobals"."Colors:Button"."ForegroundNormal" = "212,197,176";
+        "kdeglobals"."Colors:Button"."ForegroundInactive" = "120,100,80";
         
-        # Couleurs de sélection - tons brun moyen
-        "kdeglobals"."Colors:Selection"."BackgroundNormal" = "120,90,60";
-        "kdeglobals"."Colors:Selection"."BackgroundAlternate" = "80,60,40";
-        "kdeglobals"."Colors:Selection"."ForegroundNormal" = "255,250,240";
+        # Couleurs de sélection - brun cuir chaud
+        "kdeglobals"."Colors:Selection"."BackgroundNormal" = "139,111,71";
+        "kdeglobals"."Colors:Selection"."BackgroundAlternate" = "100,80,50";
+        "kdeglobals"."Colors:Selection"."ForegroundNormal" = "255,250,245";
         
-        # Accents dorés
-        "kdeglobals"."Colors:Window"."DecorationFocus" = "212,175,55";
-        "kdeglobals"."Colors:Window"."DecorationHover" = "212,175,55";
-        "kdeglobals"."Colors:Window"."ForegroundActive" = "212,175,55";
-        "kdeglobals"."Colors:Window"."ForegroundLink" = "212,175,55";
+        # Accents or champagne/rose gold
+        "kdeglobals"."Colors:Window"."DecorationFocus" = "201,168,118";
+        "kdeglobals"."Colors:Window"."DecorationHover" = "201,168,118";
+        "kdeglobals"."Colors:Window"."ForegroundActive" = "201,168,118";
+        "kdeglobals"."Colors:Window"."ForegroundLink" = "201,168,118";
         
-        "kdeglobals"."Colors:View"."DecorationFocus" = "212,175,55";
-        "kdeglobals"."Colors:View"."DecorationHover" = "212,175,55";
-        "kdeglobals"."Colors:View"."ForegroundActive" = "212,175,55";
-        "kdeglobals"."Colors:View"."ForegroundLink" = "212,175,55";
+        "kdeglobals"."Colors:View"."DecorationFocus" = "201,168,118";
+        "kdeglobals"."Colors:View"."DecorationHover" = "201,168,118";
+        "kdeglobals"."Colors:View"."ForegroundActive" = "201,168,118";
+        "kdeglobals"."Colors:View"."ForegroundLink" = "201,168,118";
         
-        "kdeglobals"."Colors:Button"."DecorationFocus" = "212,175,55";
-        "kdeglobals"."Colors:Button"."DecorationHover" = "212,175,55";
-        "kdeglobals"."Colors:Button"."ForegroundActive" = "212,175,55";
-        "kdeglobals"."Colors:Button"."ForegroundLink" = "212,175,55";
+        "kdeglobals"."Colors:Button"."DecorationFocus" = "201,168,118";
+        "kdeglobals"."Colors:Button"."DecorationHover" = "201,168,118";
+        "kdeglobals"."Colors:Button"."ForegroundActive" = "201,168,118";
+        "kdeglobals"."Colors:Button"."ForegroundLink" = "201,168,118";
         
-        "kdeglobals"."Colors:Selection"."DecorationFocus" = "212,175,55";
-        "kdeglobals"."Colors:Selection"."DecorationHover" = "212,175,55";
+        "kdeglobals"."Colors:Selection"."DecorationFocus" = "201,168,118";
+        "kdeglobals"."Colors:Selection"."DecorationHover" = "201,168,118";
         "kdeglobals"."Colors:Selection"."ForegroundActive" = "255,255,255";
-        "kdeglobals"."Colors:Selection"."ForegroundLink" = "212,175,55";
+        "kdeglobals"."Colors:Selection"."ForegroundLink" = "201,168,118";
         
-        # Couleur d'accent générale
-        "kdeglobals"."General"."AccentColor" = "212,175,55";
+        # Couleur d'accent générale - or champagne
+        "kdeglobals"."General"."AccentColor" = "201,168,118";
       };
     };
   };
