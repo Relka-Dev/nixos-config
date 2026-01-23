2c92927df6e10b38071810b5a9067db715e10729
Relka-Dev <relka.dev@gmail.com>
2026-01-24 00:45:31 +0100

    color: changed color schema for karelsvbd

--- users/karelsvbd.nix
+++ users/karelsvbd.nix
@@ -1,70 +1,69 @@
 { config, pkgs, lib, ... }:
-
 {
   users.users.karelsvbd = {
     isNormalUser = true;
-    description = "Karel";
+    description = "Karel Vilém Svoboda";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [ kdePackages.kate ];
   };
 
-  home-manager.users.karelsvbd = { pkgs, ... }: {
+  home-manager.users.karelsvbd = { pkgs, lib, ... }: {
     imports = [ ./common.nix ];
+    
+    home.file.".face.icon".source = ../avatars/karelsvbd.png;
 
-    home.file.".face.icon".source = "/etc/nixos/avatars/veks.png";
-
-    # Override: thème inspiré montre Fossil (brun chocolat/cuir + or champagne)
+    # Override: thème NixOS multicolor (gris anthracite + accents pastels)
     programs.plasma = {
-     workspace = {
-      colorScheme = "BreezeDark";
-      wallpaper = lib.mkForce "/etc/nixos/wallpapers/multicolor.png";
+      workspace = {
+        colorScheme = "BreezeDark";
+        wallpaper = lib.mkForce ../wallpapers/multicolor.png;
       };
       
       configFile = {
-        # Couleurs principales - tons chocolat/brun profond
-        "kdeglobals"."Colors:Window"."BackgroundNormal" = "28,22,16";
-        "kdeglobals"."Colors:Window"."BackgroundAlternate" = "35,28,20";
-        "kdeglobals"."Colors:Window"."ForegroundNormal" = "212,197,176";
-        "kdeglobals"."Colors:Window"."ForegroundInactive" = "120,100,80";
+        # Couleurs principales - tons gris anthracite
+        "kdeglobals"."Colors:Window"."BackgroundNormal" = "45,55,65";
+        "kdeglobals"."Colors:Window"."BackgroundAlternate" = "55,68,80";
+        "kdeglobals"."Colors:Window"."ForegroundNormal" = "220,225,230";
+        "kdeglobals"."Colors:Window"."ForegroundInactive" = "130,145,160";
         
-        "kdeglobals"."Colors:View"."BackgroundNormal" = "20,16,11";
-        "kdeglobals"."Colors:View"."BackgroundAlternate" = "30,24,18";
-        "kdeglobals"."Colors:View"."ForegroundNormal" = "212,197,176";
-        "kdeglobals"."Colors:View"."ForegroundInactive" = "120,100,80";
+        "kdeglobals"."Colors:View"."BackgroundNormal" = "35,45,55";
+        "kdeglobals"."Colors:View"."BackgroundAlternate" = "45,55,68";
+        "kdeglobals"."Colors:View"."ForegroundNormal" = "220,225,230";
+        "kdeglobals"."Colors:View"."ForegroundInactive" = "130,145,160";
         
-        "kdeglobals"."Colors:Button"."BackgroundNormal" = "40,32,24";
-        "kdeglobals"."Colors:Button"."BackgroundAlternate" = "32,26,19";
-        "kdeglobals"."Colors:Button"."ForegroundNormal" = "212,197,176";
-        "kdeglobals"."Colors:Button"."ForegroundInactive" = "120,100,80";
+        "kdeglobals"."Colors:Button"."BackgroundNormal" = "55,68,82";
+        "kdeglobals"."Colors:Button"."BackgroundAlternate" = "48,60,72";
+        "kdeglobals"."Colors:Button"."ForegroundNormal" = "220,225,230";
+        "kdeglobals"."Colors:Button"."ForegroundInactive" = "130,145,160";
         
-        # Couleurs de sélection - brun cuir chaud
-        "kdeglobals"."Colors:Selection"."BackgroundNormal" = "139,111,71";
-        "kdeglobals"."Colors:Selection"."BackgroundAlternate" = "100,80,50";
-        "kdeglobals"."Colors:Selection"."ForegroundNormal" = "255,250,245";
+        # Couleurs de sélection - turquoise/cyan du logo
+        "kdeglobals"."Colors:Selection"."BackgroundNormal" = "126,186,181";
+        "kdeglobals"."Colors:Selection"."BackgroundAlternate" = "100,160,155";
+        "kdeglobals"."Colors:Selection"."ForegroundNormal" = "25,35,45";
         
-        # Accents or champagne/rose gold
-        "kdeglobals"."Colors:Window"."DecorationFocus" = "201,168,118";
-        "kdeglobals"."Colors:Window"."DecorationHover" = "201,168,118";
-        "kdeglobals"."Colors:Window"."ForegroundActive" = "201,168,118";
-        "kdeglobals"."Colors:Window"."ForegroundLink" = "201,168,118";
+        # Accents beige/sable du logo
+        "kdeglobals"."Colors:Window"."DecorationFocus" = "228,192,122";
+        "kdeglobals"."Colors:Window"."DecorationHover" = "228,192,122";
+        "kdeglobals"."Colors:Window"."ForegroundActive" = "228,192,122";
+        "kdeglobals"."Colors:Window"."ForegroundLink" = "126,186,181";
         
-        "kdeglobals"."Colors:View"."DecorationFocus" = "201,168,118";
-        "kdeglobals"."Colors:View"."DecorationHover" = "201,168,118";
-        "kdeglobals"."Colors:View"."ForegroundActive" = "201,168,118";
-        "kdeglobals"."Colors:View"."ForegroundLink" = "201,168,118";
+        "kdeglobals"."Colors:View"."DecorationFocus" = "228,192,122";
+        "kdeglobals"."Colors:View"."DecorationHover" = "228,192,122";
+        "kdeglobals"."Colors:View"."ForegroundActive" = "228,192,122";
+        "kdeglobals"."Colors:View"."ForegroundLink" = "126,186,181";
         
-        "kdeglobals"."Colors:Button"."DecorationFocus" = "201,168,118";
-        "kdeglobals"."Colors:Button"."DecorationHover" = "201,168,118";
-        "kdeglobals"."Colors:Button"."ForegroundActive" = "201,168,118";
-        "kdeglobals"."Colors:Button"."ForegroundLink" = "201,168,118";
+        "kdeglobals"."Colors:Button"."DecorationFocus" = "228,192,122";
+        "kdeglobals"."Colors:Button"."DecorationHover" = "228,192,122";
+        "kdeglobals"."Colors:Button"."ForegroundActive" = "228,192,122";
+        "kdeglobals"."Colors:Button"."ForegroundLink" = "126,186,181";
         
-        "kdeglobals"."Colors:Selection"."DecorationFocus" = "201,168,118";
-        "kdeglobals"."Colors:Selection"."DecorationHover" = "201,168,118";
+        "kdeglobals"."Colors:Selection"."DecorationFocus" = "228,192,122";
+        "kdeglobals"."Colors:Selection"."DecorationHover" = "228,192,122";
         "kdeglobals"."Colors:Selection"."ForegroundActive" = "255,255,255";
-        "kdeglobals"."Colors:Selection"."ForegroundLink" = "201,168,118";
+        "kdeglobals"."Colors:Selection"."ForegroundLink" = "126,186,181";
         
-        # Couleur d'accent générale - or champagne
-        "kdeglobals"."General"."AccentColor" = "201,168,118";
+        # Couleur d'accent générale - beige/or du logo
+        "kdeglobals"."General"."AccentColor" = "228,192,122";
       };
     };
   };
